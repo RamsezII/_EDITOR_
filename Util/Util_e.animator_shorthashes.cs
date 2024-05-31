72e2ef4abd3904a5a9b7919b507ee23b3541f185
ram16 <thomass.montaine@gmail.com>
2024-05-31 12:00:22 +0200

    longhashes va remplacer hashes (+suppr shorthases)

---
+++ Util/Util_e.animator_shorthashes.cs
@@ -0,0 +1,58 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public static partial class Util_e
+{
+    [MenuItem("Assets/" + nameof(Util_e) + "." + nameof(LogAnimatorInfos_short))]
+    static void LogAnimatorInfos_shorthash() => LogAnimatorInfos_shorthash((AnimatorController)Selection.activeObject);
+
+    [MenuItem("CONTEXT/" + nameof(Animator) + "/" + nameof(Util_e) + "." + nameof(LogAnimatorInfos_short))]
+    static void LogAnimatorInfos_short(MenuCommand command) => LogAnimatorInfos_shorthash(((Animator)command.context).runtimeAnimatorController as AnimatorController);
+
+    [System.Obsolete("Use LogAnimatorInfos_fullhash instead")]
+    public static void LogAnimatorInfos_shorthash(this AnimatorController animator)
+    {
+        string layersEnum = "public enum Layers : byte { ";
+        var statesEnums = new System.Collections.Generic.List<string>();
+
+        foreach (AnimatorControllerLayer layer in animator.layers)
+        {
+            layersEnum += layer.name + ", ";
+
+            string statesEnum = "public enum " + layer.name + "States\n{\n";
+
+            RefAllStates(layer.stateMachine, ref statesEnum);
+
+            statesEnums.Add(statesEnum + "}");
+        }
+
+        layersEnum += " _last_ }\n\n";
+
+        foreach (string statesEnum in statesEnums)
+            layersEnum += statesEnum + "\n\n";
+
+        layersEnum += "public enum Parameters\n{\n";
+
+        foreach (var p in animator.parameters)
+            layersEnum += "    " + p.name + " = " + p.nameHash + ",\n";
+
+        layersEnum += "}";
+
+        layersEnum.WriteToClipboard();
+        Debug.Log(layersEnum);
+
+        static void RefAllStates(AnimatorStateMachine stateMachine, ref string log, string branch = default)
+        {
+            if (stateMachine.stateMachines != null)
+                foreach (var subStateMachine in stateMachine.stateMachines)
+                    RefAllStates(subStateMachine.stateMachine, ref log, branch + subStateMachine.stateMachine.name + "_");
+
+            if (stateMachine.states != null)
+                foreach (var subState in stateMachine.states)
+                    log += "    " + branch + subState.state.name + " = " + subState.state.nameHash + ",\n";
+        }
+    }
+}
+#endif
---
+++ Util/Util_e.layouts.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static partial class Util_e
+{
+    [MenuItem("CONTEXT/" + nameof(VerticalLayoutGroup) + "/" + nameof(LogPreferredHeight))]
+    public static void LogPreferredHeight(MenuCommand command)
+    {
+        VerticalLayoutGroup verticalLayoutGroup = (VerticalLayoutGroup)command.context;
+        Debug.Log($"Preferred Height: {verticalLayoutGroup.preferredHeight}");
+    }
+}
+#endif
