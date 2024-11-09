f9d2e91d4efc73d2e325f66d01e1497361eaf9bb
ram16 <thomass.montaine@gmail.com>
2024-11-09 02:12:28 +0100

    Update Util_e.layouts.cs

--- Util/Util_e.layouts.cs
+++ Util/Util_e.layouts.cs
@@ -13,15 +13,7 @@ public static partial class Util_e
     }
 
     [MenuItem("CONTEXT/" + nameof(LayoutGroup) + "/" + nameof(AutoHeight))]
-    public static void AutoHeight(MenuCommand command)
-    {
-        LayoutGroup layoutGroup = (LayoutGroup)command.context;
-        RectTransform rT = (RectTransform)layoutGroup.transform;
-
-        Vector2 sizeDelta = rT.sizeDelta;
-        sizeDelta.y = layoutGroup.preferredHeight;
-        rT.sizeDelta = sizeDelta;
-    }
+    public static void AutoHeight(MenuCommand command) => ((LayoutGroup)command.context).AutoSize();
 
     [MenuItem("CONTEXT/" + nameof(LayoutGroup) + "/" + nameof(AutoHeightParent))]
     public static void AutoHeightParent(MenuCommand command)
