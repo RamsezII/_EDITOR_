0a35365a48a19a6fa4f6656a793e9695107158e9
ram16 <thomass.montaine@gmail.com>
2025-04-05 14:47:42 +0200

    nice

---
+++ Processors/AutoRefreshBeforePlay.cs
@@ -0,0 +1,33 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace _EDITOR_
+{
+    internal static class AutoRefreshBeforePlay
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void OnBeforeSceneLoad()
+        {
+            EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.ExitingEditMode)
+            {
+                Debug.Log("🔁 Auto Refresh avant Play...".ToSubLog());
+
+                double start = Util.TotalMilliseconds;
+                AssetDatabase.Refresh();
+                double stop = Util.TotalMilliseconds;
+
+                Debug.Log($"🔁 Auto Refresh terminé en {(stop - start) / 1000.0}s.".ToSubLog());
+            }
+        }
+    }
+}
+#endif
